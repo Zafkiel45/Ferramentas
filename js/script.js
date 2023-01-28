9700eaa9958fae72c775078915d2420779981e47
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-01-28 19:07:15 -0300

    uptade imc incomplete 90%

--- js/script.js
+++ js/script.js
@@ -7,3 +7,39 @@ function mostrar() {
         imc.style.display = 'none'
     }
 }
+
+function calcular() {  
+    let altura = document.getElementById('altura').value;
+    let peso = document.getElementById('peso').value;
+    let res = document.getElementById('res');
+    let gen = document.getElementsByName('sex');
+    let num1 = Number(altura);
+    let num2 = Number(peso);
+    let total = (num2 / num1 ** 2).toFixed(2);
+
+    if(gen[0].checked) {
+        res.innerHTML = `Você é um homem e  seu IMC é: ${total}<br>
+        - <span class="perigo"> Abaixo do peso: < 20.7 </span>
+        <br>
+        - <span class="normal">Peso normal: 20.7 - 26.4 </span>
+        <br>
+        - <span class="alerta">Marginalmente acima do peso: 26.4 - 27.8 </span>
+        <br>
+        - <span class="alerta">Acima do peso ideal: 27.8 - 31.1 </span>
+        <br> 
+        - <span class="perigo"> Obeso: > 31.1 </span>
+        `
+    } else if(gen[1].checked) {
+        res.innerHTML = `Você é uma mulher e  seu IMC é: ${total}<br>
+        - <span class="perigo"> Abaixo do peso: < 20.7 </span>
+        <br>
+        - <span class="normal">Peso normal: 20.7 - 26.4 </span>
+        <br>
+        - <span class="alerta">Marginalmente acima do peso: 26.4 - 27.8 </span>
+        <br>
+        - <span class="alerta">Acima do peso ideal: 27.8 - 31.1 </span>
+        <br> 
+        - <span class="perigo"> Obeso: > 31.1 </span>
+        `
+    }
+}
